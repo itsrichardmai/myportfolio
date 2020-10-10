78ac92a938d9a56015092538c85495fa28be1397
Richard M <richardmaichs276@gmail.com>
2020-10-10 15:47:00 -0400

    Updated projects-timeline and work

--- src/components/projects-timeline/projects-timeline.jsx
+++ src/components/projects-timeline/projects-timeline.jsx
@@ -29,16 +29,17 @@ import "./projects-timeline.styles.css";
 const TimeLine = () => {
   return (
     <div id="projects">
-      <h1 className="pt-3 text-center font-details-b pb-3">PROJECTS</h1>
+      <h1 className="pt-3 text-center font-details-b pb-3"
+      style={{color:"black"}}>PROJECTS</h1>
       <Timeline>
         <Events>
           {/* Project: Get GitHub Info */}
           <ImageEvent
             date="10/09/2020"
             className="text-center"
-            text="Get GitHub Info"
-            src={L_GetGitHubInfo}
-            alt="Get GitHub Info"
+            text="Placeholder text"
+            // src={L_GetGitHubInfo}
+            alt="Placeholder text"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -54,14 +55,12 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong> An app that searches
-                        people on GitHub with non-private account and see all
-                        their details using GitHub API
+                        <strong>Description:</strong> An application description
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
-                          <li>Search any Github profile</li>
-                          <li>Real-time API calls</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -143,9 +142,9 @@ const TimeLine = () => {
           <ImageEvent
             date="21/01/2020"
             className="text-center"
-            text="Smart Brain Face Detect app"
-            src={L_SmartBrain}
-            alt="Smart Brain Face Detect"
+            text="Placeholder Text"
+            // src={L_SmartBrain}
+            alt="Placeholder Text"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -161,14 +160,13 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong> An app that detects face
-                        in a picture.
+                        <strong>Description:</strong> An application description
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
-                          <li>Register new user</li>
-                          <li>Signin exiting user</li>
-                          <li>Keep tracks of entries for each user</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -220,18 +218,6 @@ const TimeLine = () => {
                         </ul>
                         <hr />
                         <em>
-                          <strong>SignUp/ Signin:</strong>
-                          <br />
-                          <br />
-                          You can <strong>register</strong> as new user or{" "}
-                          <strong>log in</strong> using the demo account below.
-                          <br />
-                          <br />
-                          <strong>Demo Account Details:</strong>
-                          <br />
-                          email: demo@demo.com
-                          <br />
-                          password: demo
                         </em>
                       </Card.Body>
                     </Accordion.Collapse>
@@ -240,19 +226,19 @@ const TimeLine = () => {
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
                 <UrlButton
-                  href="https://smart-face-detect-app.herokuapp.com/"
+                  href="#"
                   target="_blank"
                 >
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
-                  href="https://github.com/akjha96/Smart_Brain"
+                  href="#"
                   target="_blank"
                 >
                   SOURCE CODE
                 </UrlButton>
                 <UrlButton
-                  href="https://www.youtube.com/watch?v=ZZYSpi_blL0"
+                  href="#"
                   target="_blank"
                 >
                   WATCH VIDEO
@@ -266,9 +252,9 @@ const TimeLine = () => {
           <ImageEvent
             date="14/01/2020"
             className="text-center"
-            text="RoboFriends"
-            src={L_RoboFriends}
-            alt="Robo Friends"
+            text="Placeholder Text"
+            // src={L_RoboFriends}
+            alt="Placeholder text"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -284,14 +270,13 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong> An app that allows to
-                        search through robots fetched in real-time with users.
+                        <strong>Description:</strong>An application description
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
-                          <li>Search through Robots</li>
-                          <li>Users and Robots being fetched via API call</li>
-                          <li>Usage of Redux store</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -359,13 +344,13 @@ const TimeLine = () => {
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
                 <UrlButton
-                  href="https://akjha96.github.io/RoboFriends/"
+                  href="#"
                   target="_blank"
                 >
                   SEE LIVE
                 </UrlButton>
                 <UrlButton
-                  href="https://github.com/akjha96/RoboFriends"
+                  href="#"
                   target="_blank"
                 >
                   SOURCE CODE
@@ -379,9 +364,9 @@ const TimeLine = () => {
           <ImageEvent
             date="15/12/2019"
             className="text-center"
-            text="Product Hunt Clone"
-            src={L_ProductHuntClone}
-            alt="Product Hunt Clone"
+            text="Placeholder text"
+            // src={L_ProductHuntClone}
+            alt="Placeholder text"
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -397,17 +382,16 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong> This website is a clone of
-                        Product Hunt built using Django.
+                        <strong>Description:</strong> An application description
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
-                          <li>Register as new user</li>
-                          <li>Signin as existing user</li>
-                          <li>Raise a Product for people to see and Vote</li>
-                          <li>Delete a product</li>
-                          <li>Like and Dislike a product</li>
-                          <li>Fully funtional Administration panel</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -475,7 +459,7 @@ const TimeLine = () => {
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
                 <UrlButton
-                  href="https://github.com/akjha96/product_hunt_clone"
+                  href="#"
                   target="_blank"
                 >
                   SOURCE CODE
@@ -488,9 +472,9 @@ const TimeLine = () => {
           <ImageEvent
             date="07/12/2019"
             className="text-center"
-            text="Portfolio using Django"
-            src={L_PortfolioUsingDjango}
-            alt="Portfolio using Django"
+            text="Placeholder text"
+            // src={L_PortfolioUsingDjango}
+            alt="Placeholder text"listing placeholder
           >
             <div className="d-flex justify-content-between flex-column mt-1">
               <div>
@@ -506,18 +490,13 @@ const TimeLine = () => {
 
                     <Accordion.Collapse eventKey="0" className="text-left">
                       <Card.Body>
-                        <strong>Description:</strong> This is my first portfolio
-                        website built using Django.
+                        <strong>Description:</strong> An application description
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
-                          <li>Send emails</li>
-                          <li>View my projects</li>
-                          <li>Write a blog</li>
-                          <li>
-                            Hosted using Digital ocean under www.akjfun.com
-                          </li>
-                          <li>Fully funtional Administration panel</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
+                          <li>listing placeholder</li>
                         </ul>
                         <hr />
                         <strong>Tech used:</strong>
@@ -608,7 +587,7 @@ const TimeLine = () => {
                 </Accordion>
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
-                <UrlButton href="https://youtu.be/PCwEuW4OmWA" target="_blank">
+                <UrlButton href="#" target="_blank">
                   WATCH VIDEO
                 </UrlButton>
               </div>
